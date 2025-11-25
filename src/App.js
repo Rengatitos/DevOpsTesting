572b3edfd7b436cf92f7b83b35762eb89c3d9a69
Camila <22200168@ue.edu.pe>
2025-11-24 20:24:36 -0500

    probando cambio

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          DevOps Testing  <code>CI-CD</code> ESAN Project
+        </p>
+        semana 15
+          <a href="https://www.esan.edu.pe/">learn react</a>
+          <p>Prueba de SOftware</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
